cbaa6e1a1959c848d8bb0704045158779d21932e
claudio vigliarolo <claviglia@yahoo.it>
2020-02-01 01:48:35 +0100

    hopefully

--- src/index.js
+++ src/index.js
@@ -9,8 +9,19 @@ const morgan = require('morgan');
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('../frontend/build'));
+
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'static', 'index.html'));
+    });
+}
+
 app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '../frontend'));
 app.use(express.json());
 app.use(cors());
 app.use(
