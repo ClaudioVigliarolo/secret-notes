2eaff482e30e43e74297487a3e7132684a3762ca
claudio vigliarolo <claviglia@yahoo.it>
2020-02-02 14:25:55 +0100

    new comm

---
+++ client/build/precache-manifest.cb052d81a133abab764fec478b954fb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26d56f00df435d50f6c113c1a951c08a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c5f8d682faa4db5a51c8",
+    "url": "./static/css/2.0996cbbd.chunk.css"
+  },
+  {
+    "revision": "5bcbeb4371e32b2ef431",
+    "url": "./static/css/main.1c7a9178.chunk.css"
+  },
+  {
+    "revision": "c5f8d682faa4db5a51c8",
+    "url": "./static/js/2.4e92a58b.chunk.js"
+  },
+  {
+    "revision": "5bcbeb4371e32b2ef431",
+    "url": "./static/js/main.d7f1e51d.chunk.js"
+  },
+  {
+    "revision": "c7949412f7f151a27416",
+    "url": "./static/js/runtime-main.7e3ba4a1.js"
+  }
+]);
