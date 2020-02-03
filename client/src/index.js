64c11926d39226d905f7ee4daf15cb8c8a6f729c
claudio vigliarolo <claviglia@yahoo.it>
2020-02-03 09:16:51 +0100

    added compression and express history

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { withRouter } from 'react-router-dom';
-import { Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
 const AppWithRouter = withRouter(App);
