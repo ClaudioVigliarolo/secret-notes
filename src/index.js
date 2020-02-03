1c3e2488e3beab9c1a078ad95aa2d53061078d66
claudio vigliarolo <claviglia@yahoo.it>
2020-02-03 08:58:01 +0100

    added compression and express history

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { PORT } = require('../config/config')
+const { PORT } = require('../config/config');
 const express = require('express');
 require('./db/mongoose');
 const bodyParser = require('body-parser');
@@ -6,8 +6,10 @@ const fileUpload = require('express-fileupload');
 const userRouter = require('./routers/user');
 const NotebookRouter = require('./routers/noteBook');
 const cors = require('cors');
+const history = require('express-history-api-fallback');
+const compression = require('compression');
 const morgan = require('morgan');
-const path = require('path')
+const path = require('path');
 const app = express();
 
 // Exprees will serve up production assets
@@ -15,19 +17,21 @@ app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.json());
+app.use(compression());
 app.use(cors());
 app.use(
-    fileUpload({
-        createParentPath: true,
-        limits: { fileSize: 50 * 1024 * 1024 }
-    })
+  fileUpload({
+    createParentPath: true,
+    limits: { fileSize: 50 * 1024 * 1024 }
+  })
 );
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(userRouter);
 app.use(morgan('dev'));
+app.use(history());
 app.use(NotebookRouter);
 
 app.listen(PORT, () => {
-    console.log('Server is up on PORT ' + PORT);
+  console.log('Server is up on PORT ' + PORT);
 });
