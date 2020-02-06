3a3cf8fe0341cfff4e33a9edb4b7795af7d5c0eb
claudio vigliarolo <claviglia@yahoo.it>
2020-02-06 08:27:14 +0100

    added react lazy

--- client/src/components/layout/SideBar/Sidebar.js
+++ client/src/components/layout/SideBar/Sidebar.js
@@ -1,8 +1,8 @@
-import React from "react";
-import classNames from "classnames";
-import { Col } from "shards-react";
-import SidebarHeader from "./components/SidebarHeader";
-import SideNotebooks from "./components/SideNotebooks";
+import React from 'react';
+import classNames from 'classnames';
+import { Col } from 'shards-react';
+import SidebarHeader from './components/SidebarHeader';
+import SideNotebooks from './components/SideNotebooks';
 import MenuIcon from '@material-ui/icons/Menu';
 
 class MainSidebar extends React.Component {
@@ -13,35 +13,43 @@ class MainSidebar extends React.Component {
       userData: this.props.userData,
       token: this.props.token,
       notebooks: this.props.notebooks,
-      isOpen: true
+      isOpen: false
     };
   }
 
-  toggleOpen = () => (this.setState({ isOpen: !this.state.isOpen }))
-
+  toggleOpen = () => this.setState({ isOpen: !this.state.isOpen });
 
   render() {
-
     const { userData, token, notebooks } = this.state;
     const classes = classNames(
-      "main-sidebar",
-      "px-0",
-      "col-12",
-      this.state.isOpen ? "open" : ""
+      'main-sidebar',
+      'px-0',
+      'col-12',
+      this.state.isOpen ? 'open' : ''
     );
     return (
       <div>
-        <Col
-          tag="aside"
-          className={classes}
-          lg={{ size: 2 }}
-          md={{ size: 3 }}
-        >
+        <Col tag="aside" className={classes} lg={{ size: 2 }} md={{ size: 3 }}>
           <SidebarHeader toggleOpen={this.toggleOpen} />
-          <SideNotebooks userData={userData} token={token} notebooks={notebooks}
-            loadNotes={this.props.loadNotes} key={userData && token && notebooks[0] && notebooks[0]._id && this.props.currentNotebookIndex && this.props.isEditable} currentNotebookIndex={this.props.currentNotebookIndex}
-            onSubmit={this.onCreate} refreshNotebooks={this.props.refreshNotebooks} toggleEditNotebook={this.props.toggleEditNotebook}
-            isEditable={this.props.isEditable} />
+          <SideNotebooks
+            userData={userData}
+            token={token}
+            notebooks={notebooks}
+            loadNotes={this.props.loadNotes}
+            key={
+              userData &&
+              token &&
+              notebooks[0] &&
+              notebooks[0]._id &&
+              this.props.currentNotebookIndex &&
+              this.props.isEditable
+            }
+            currentNotebookIndex={this.props.currentNotebookIndex}
+            onSubmit={this.onCreate}
+            refreshNotebooks={this.props.refreshNotebooks}
+            toggleEditNotebook={this.props.toggleEditNotebook}
+            isEditable={this.props.isEditable}
+          />
         </Col>
         <MenuIcon className="menu-icon" onClick={this.toggleOpen} />
       </div>
