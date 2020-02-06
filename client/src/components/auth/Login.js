c486122ad4e15735d460c5f8744e5fcb0c43b207
claudio vigliarolo <claviglia@yahoo.it>
2020-02-06 08:27:14 +0100

    added react lazy

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,39 +1,34 @@
 import React, { Component } from 'react';
 import { withRouter, Link } from 'react-router-dom';
-import { loginUser } from '../../api/api'
+import { loginUser } from '../../api/api';
 class Login extends Component {
   state = {
     email: '',
     password: '',
     errors: [],
-    loading: '',
-  }
+    loading: ''
+  };
 
-
-  displayErrors = errors =>
-    errors.map((error, i) => error.message);
+  displayErrors = errors => errors.map((error, i) => error.message);
 
   handleChange = event => {
     this.setState({ [event.target.name]: event.target.value });
   };
 
-  handleInputError = () => (this.state.errors.length > 0
-    ? "form-control is-invalid"
-    : "form-control"
-  )
+  handleInputError = () =>
+    this.state.errors.length > 0 ? 'form-control is-invalid' : 'form-control';
   /*
 
   */
 
-  handleSubmit = (e) => {
+  handleSubmit = e => {
     e.preventDefault();
     this.loginUser(this.state);
-  }
-
+  };
 
   loginUser = ({ email, password }) => {
     loginUser(email, password)
-      .then((res) => {
+      .then(res => {
         this.props.history.push({
           pathname: '/secretNotes',
           userToken: res.data.token // pass test data as a props
@@ -41,22 +36,16 @@ class Login extends Component {
       })
       .catch(() => {
         let errors = [];
-        const error = { message: "Unable To Login" };
+        const error = { message: 'Unable To Login' };
         this.setState({ errors: errors.concat(error) });
       });
-  }
-
+  };
 
   render() {
-
-    const {
-      email,
-      password,
-      errors
-    } = this.state;
+    const { email, password, errors } = this.state;
 
     return (
-      <div className="div-center form-responsive-login" >
+      <div className="div-center form-responsive-login">
         <div className="card card-small mb-4">
           <div className="card-header border-bottom">
             <h4 className="text-center">Login</h4>
@@ -68,29 +57,66 @@ class Login extends Component {
                   <form onSubmit={this.handleSubmit}>
                     <div className="flex-column-center">
                       <div className="form-group col-md-6  ">
-                        <label htmlFor="feEmailAddress" onChange={this.handleChange}>Email</label>
-                        <input name="email" type="email" className={this.handleInputError(errors)} id="feEmailAddress" placeholder="Email" value={email} onChange={this.handleChange} />
+                        <label
+                          htmlFor="feEmailAddress"
+                          onChange={this.handleChange}
+                        >
+                          Email
+                        </label>
+                        <input
+                          name="email"
+                          type="email"
+                          className={this.handleInputError(errors)}
+                          id="feEmailAddress"
+                          placeholder="Email"
+                          value={email}
+                          onChange={this.handleChange}
+                        />
                       </div>
-                      <div className="form-group col-md-6" style={{ marginTop: 10 }} >
+                      <div
+                        className="form-group col-md-6"
+                        style={{ marginTop: 10 }}
+                      >
                         <label htmlFor="fePassword">Password</label>
-
-                        <input name="password" type="password" className={this.handleInputError(errors)} id="fePassword" onChange={this.handleChange} value={password} placeholder="Password" /> </div>
+                        <input
+                          name="password"
+                          type="password"
+                          className={this.handleInputError(errors)}
+                          id="fePassword"
+                          onChange={this.handleChange}
+                          value={password}
+                          placeholder="Password"
+                        />{' '}
+                      </div>
                     </div>
-                    {<h6 className="text-center" style={{ color: 'red' }}>{this.displayErrors(errors)}</h6>}
+                    {
+                      <h6 className="text-center" style={{ color: 'red' }}>
+                        {this.displayErrors(errors)}
+                      </h6>
+                    }
                     <div className="flex-center">
-                      <button type="submit" className="btn btn-accent" style={{ marginTop: 10 }}>Sign In</button>
+                      <button
+                        type="submit"
+                        className="btn btn-accent"
+                        style={{ marginTop: 10 }}
+                      >
+                        Sign In
+                      </button>
                     </div>
                   </form>
-                  <Link to="/register" className="bottom-nav-text "> <h6> Register <i className="arrow right bottom-nav-text-i"></i> </h6>
-                  </Link>
                 </div>
               </div>
             </li>
           </ul>
         </div>
-
+        <Link to="/register" className="bottom-nav-text ">
+          {' '}
+          <h6>
+            {' '}
+            Register <i className="arrow right bottom-nav-text-i"></i>{' '}
+          </h6>
+        </Link>
       </div>
-
     );
   }
 }
