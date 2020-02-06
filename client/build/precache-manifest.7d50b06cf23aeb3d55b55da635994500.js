f5985069ac658a60fce3b4b4e1b931f182c212aa
claudio vigliarolo <claviglia@yahoo.it>
2020-02-06 08:57:11 +0100

    added react lazy

---
+++ client/build/precache-manifest.7d50b06cf23aeb3d55b55da635994500.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adb8b8395694171794744d419e9da947",
+    "url": "./index.html"
+  },
+  {
+    "revision": "99aca4be916744dbbb9a",
+    "url": "./static/css/3.0996cbbd.chunk.css"
+  },
+  {
+    "revision": "0385e766c951f0e99cc3",
+    "url": "./static/css/4.0758dbb3.chunk.css"
+  },
+  {
+    "revision": "1764afebe3aafe01ffc0",
+    "url": "./static/css/main.41ac936c.chunk.css"
+  },
+  {
+    "revision": "d279368eba22e3057d19",
+    "url": "./static/js/2.5ea7189e.chunk.js"
+  },
+  {
+    "revision": "99aca4be916744dbbb9a",
+    "url": "./static/js/3.db1d49b7.chunk.js"
+  },
+  {
+    "revision": "0385e766c951f0e99cc3",
+    "url": "./static/js/4.eafe203c.chunk.js"
+  },
+  {
+    "revision": "1764afebe3aafe01ffc0",
+    "url": "./static/js/main.d67c7398.chunk.js"
+  },
+  {
+    "revision": "a0002b8e0d736f13b9d3",
+    "url": "./static/js/runtime-main.3adb6d58.js"
+  }
+]);
--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from 'react';
+/*import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
@@ -7,6 +7,35 @@ import NotFound from './components/NotFound/NotFound';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles-template/styles/styles.1.1.0.min.css';
 import './styles/app.css';
+
+export default function() {
+  return (
+    <Switch>
+      <Route exact path="/" render={() => <Redirect to="/login" />} />
+      <Route
+        exact
+        path="/register"
+        render={() => <Register component={Register} />}
+      />
+      <Route exact path="/login" render={() => <Login component={Login} />} />
+      <Route path="/secretNotes" render={() => <Main component={Main} />} />
+      <Route path="" component={NotFound} />
+    </Switch>
+  );
+}
+
+*/
+import React, { lazy, Suspense } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+//import Main from './components/main/Main';
+import NotFound from './components/NotFound/NotFound';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles-template/styles/styles.1.1.0.min.css';
+import './styles/app.css';
+
+const Main = lazy(() => import('./components/main/Main'));
 
 export default function() {
   return (
