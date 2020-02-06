dff807b3f429c63c0d45604ef2b7aa7caeae6ff6
claudio vigliarolo <claviglia@yahoo.it>
2020-02-06 08:27:14 +0100

    added react lazy

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,36 @@
+import React, { lazy, Suspense } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import Main from './components/main/Main';
+import NotFound from './components/NotFound/NotFound';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles-template/styles/styles.1.1.0.min.css';
+import './styles/app.css';
 
-
-
-import React from "react";
-import { Route, Switch, Redirect } from "react-router-dom";
-import Login from './components/auth/Login'
-import Register from './components/auth/Register'
-import Main from './components/main/Main'
-import NotFound from './components/NotFound/NotFound'
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./styles-template/styles/styles.1.1.0.min.css"
-import "./styles/app.css"
-
-
-export default function () {
+export default function() {
   return (
     <Switch>
-      <Route exact path="/" render={() => (
-        <Redirect to="/login" />
-      )} />
-      <Route exact path="/register" render={() => (<Register component={Register} />)} />
-      <Route exact path="/login" render={() => (<Login component={Login} />)} />
-      <Route path="/secretNotes" render={() => (<Main component={Main} />)} />
+      <Route exact path="/" render={() => <Redirect to="/login" />} />
+      <Route
+        exact
+        path="/register"
+        render={() => <Register component={Register} />}
+      />
+      <Route exact path="/login" render={() => <Login component={Login} />} />
+      <Route
+        path="/secretNotes"
+        render={() => <Main component={WaitingComponent(Main)} />}
+      />
       <Route path="" component={NotFound} />
     </Switch>
+  );
+}
 
-  )
-
+function WaitingComponent(Component) {
+  return props => (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Component {...props} />
+    </Suspense>
+  );
 }
