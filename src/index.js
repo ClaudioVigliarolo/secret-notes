0d491081370d8d0ea22997d90ce781f051010401
claudio vigliarolo <claviglia@yahoo.it>
2020-02-08 13:57:05 +0100

    added history

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const { PORT } = require('../config/config');
 const express = require('express');
 require('./db/mongoose');
-const history = require('express-history-api-fallback');
+const fallback = require('express-history-api-fallback');
 const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
 const userRouter = require('./routers/user');
@@ -27,7 +27,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(morgan('dev'));
-app.use('index.html', path.join(__dirname, '..', 'client', 'build'));
+app.use(
+  fallback('index.html', {
+    root: path.join(__dirname, '..', 'client', 'build', 'index.html')
+  })
+);
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 app.use(userRouter);
