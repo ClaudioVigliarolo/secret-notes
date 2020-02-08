d6de4499faacf4b646c867705f97f5531aa9c170
claudio vigliarolo <claviglia@yahoo.it>
2020-02-08 13:51:35 +0100

    added history

--- src/index.js
+++ src/index.js
@@ -27,7 +27,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(morgan('dev'));
-app.use(history());
+app.use(
+  history({
+    // verbose: true
+  })
+);
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 app.use(userRouter);
