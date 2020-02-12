a3d4510b62749fa9eb85b8c11e50725cac4b3d62
claudio vigliarolo <claviglia@yahoo.it>
2020-02-12 14:10:50 +0100

    improved registration

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,12 @@
 import React, { lazy, Suspense } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import Login from './components/auth/Login';
+import Main from './components/main/Main';
 import Register from './components/auth/Register';
 import NotFound from './components/NotFound/NotFound';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles-template/styles/styles.1.1.0.min.css';
 import './styles/app.css';
-
-const Main = lazy(() => import('./components/main/Main'));
-
 export default function() {
   return (
     <Switch>
@@ -19,19 +17,8 @@ export default function() {
         render={() => <Register component={Register} />}
       />
       <Route exact path="/login" render={() => <Login component={Login} />} />
-      <Route
-        path="/secretNotes"
-        render={() => <Main component={WaitingComponent(Main)} />}
-      />
+      <Route path="/secretNotes" render={() => <Main component={Main} />} />
       <Route path="" component={NotFound} />
     </Switch>
-  );
-}
-
-function WaitingComponent(Component) {
-  return props => (
-    <Suspense fallback={<div>Loading...</div>}>
-      <Component {...props} />
-    </Suspense>
   );
 }
