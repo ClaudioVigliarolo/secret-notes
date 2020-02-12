d8942df7f4531cc29b22405629e6e1ef7219feee
claudio vigliarolo <claviglia@yahoo.it>
2020-02-12 14:16:10 +0100

    improved registration

--- client/build/precache-manifest.2df414ff8f481ed172f51368ad9c73da.js
+++ client/build/precache-manifest.2df414ff8f481ed172f51368ad9c73da.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "412ed2c5633e84d95606b8d718805867",
+    "revision": "56e0bc1a6f17991d8ffe2252e12ff1c3",
     "url": "./index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/css/2.0996cbbd.chunk.css"
   },
   {
-    "revision": "4fd1a2f56e6d06e6afa4",
+    "revision": "b933ab973e461373d789",
     "url": "./static/css/main.0e5d8cd4.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.4e92a58b.chunk.js"
   },
   {
-    "revision": "4fd1a2f56e6d06e6afa4",
-    "url": "./static/js/main.7b57a07b.chunk.js"
+    "revision": "b933ab973e461373d789",
+    "url": "./static/js/main.29336149.chunk.js"
   },
   {
     "revision": "c7949412f7f151a27416",
