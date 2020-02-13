14428cece8090dc903c6ff458452e0fc37bcf234
claudio vigliarolo <claviglia@yahoo.it>
2020-02-13 18:41:06 +0100

    added redis

--- config/config.js
+++ config/config.js
@@ -5,8 +5,8 @@ module.exports = {
 
   MONGO_URL: process.env.NODE_ENV === 'production' ? process.env.MONGO_URL : 'mongodb://127.0.0.1:27017/node-app-secretnotes',
   PORT: process.env.PORT ? process.env.PORT : 5000,
-  JWT_CODE: process.env.JWT_CODE || 'kebbaro4ever'
-
+  JWT_CODE: process.env.JWT_CODE || 'kebbaro4ever',
+  REDIS_URL: process.env.NODE_ENV === 'production' ? process.env.REDIS_URL : ''
 
 
 }
